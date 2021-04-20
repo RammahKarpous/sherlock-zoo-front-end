e95bbde1059bcdcc0ea23b374f33762eea570144
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-04-20 14:02:25 +0100

    Replaced the h1 with the Heading component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,6 +5,7 @@ import Buttons from '../../components/commonly-used-elements/Buttons';
 import AboutTheConservation from '../../components/AboutTheConservation';
 import meerkats from '../../assets/images/home-conservation-img.png'
 import Animals from '../../components/animals/Animals';
+import Heading from '../../components/commonly-used-elements/Headings';
 
 const Home = () => {
     return (
@@ -13,7 +14,7 @@ const Home = () => {
                 <div 
                     className="p-10 md:p-20 w-5/6 md:w-4/6 lg:w-3/6 xl:w-2/6 bg-body-default absolute left-1/2 transform -translate-x-1/2 bottom-4 md:left-10 md:translate-x-0 md:bottom-auto md:top-24">
 
-                    <h1 className="text-3xl md:text-5xl font-bold text-green-dark mb-3">Plan a day at Sherlock Zoos</h1>
+                    <Heading size="large" level="1" text="Plan a day at Sherlock Zoos" />
 
                     <p className="mb-3">Plan your day, visit our zoo and be friends with all of our animals. When you come along, not only you can become friends with them, but you can also discover the new world of wild life and learn what we do!</p>
 
