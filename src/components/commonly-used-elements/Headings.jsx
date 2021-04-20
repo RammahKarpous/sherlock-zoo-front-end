360bb553a650e75d6b25352a315ce835a20507df
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-04-20 14:01:50 +0100

    Update Headings.jsx

--- src/components/commonly-used-elements/Headings.jsx
+++ src/components/commonly-used-elements/Headings.jsx
@@ -2,12 +2,12 @@ const Heading = ({ text, colour, extraClasses, size, level }) => {
 	let Tag = 'h' + level;
 
 	return (
-		<Tag className={`text-${colour} font-bold text-${size} mb-7 ${extraClasses}`}>{text}</Tag>
+		<Tag className={`text-${colour} font-bold text-${size} mb-7 leading-none ${extraClasses}`}>{text}</Tag>
 	);
 };
 
 Heading.defaultProps = {
-	level: '2',
+	level: 'p',
 	size: 'medium',
 	text: 'Add some text. use text="" prop.',
 	colour: 'green-dark',
