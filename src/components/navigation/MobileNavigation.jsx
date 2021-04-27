af85205f07702ea3b7a4ac04f523db4c31a688d3
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-04-27 13:17:43 +0100

    Created the toggle functionality for the mobile navigation

--- src/components/navigation/MobileNavigation.jsx
+++ src/components/navigation/MobileNavigation.jsx
@@ -1,25 +1,32 @@
+import { useState } from 'react';
 import { Link } from "react-router-dom";
 import { links } from "./links";
 
 const MobileNavigation = () => {
+
+	const [isShowing, setIsShowing] = useState(false);
+
 	const showMenu = () => {
-		console.log("Hello world");
+		setIsShowing(!isShowing);
 	};
 
 	return (
 		<nav className="fixed bottom-0 w-full z-10">
-			<ul className="flex md:hidden w-full justify-between bg-body-default">
+			<ul className="flex lg:hidden w-full justify-between bg-body-default">
 				{links.slice(0, 3).map((link, i) => (
 					<li key={i} className="w-1/4">
-						<Link className="p-5 inline-block w-full" to={link.to}>
+						<Link className="p-5 inline-block w-full text-center" to={link.to}>
 							{link.name}
 						</Link>
 					</li>
 				))}
 
-				<li className="bg-green-button p-5 w-1/4" onClick={() => showMenu()}>
+				<li className="bg-green-button p-5 w-1/4 text-center" onClick={() => showMenu()}>
 					More
-					<ul className="fixed flex-col bg-white bottom-20 right-4 flex w-40 shadow-lg rounded-md duration-75">
+					<ul className={`
+						fixed flex-col bg-white bottom-20 flex right-4 w-40 shadow-lg rounded-md transition duration-75
+						${isShowing ? 'opacity-100 visible right-4' : 'opacity-0 invisible right-0'} 
+					`}>
 						{links.slice(3, 7).map((link, i) => (
 							<li key={i} className="w-full">
 								<Link
