31fc1d55cd1162380c55bcdf7016541c28e7ca6b
Rammah Karpous <35765926+RammahKarpous@users.noreply.github.com>
2021-05-18 10:03:29 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,7 @@
 const Header = ({children, image}) => {
 	return (
         <div 
-            style={{ 
-                background: `url('${image}') no-repeat center center / cover`,
-                height: '800px'
-            }}>
+            style={{background: `url('${image}') no-repeat center center / cover`}} className="h-header">
 
             {children}
         </div>
